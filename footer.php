dccb309bac532b75071339f5f736c01f93859e1d
安和 <42889600+AHCorn@users.noreply.github.com>
2020-03-31 16:57:33 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,3 @@
+<script src="https://cdn.staticfile.org/jquery/3.4.1/jquery.js"></script>
+<script src="https://cdn.staticfile.org/jqueryui/1.12.1/jquery-ui.js"></script>
+<script src="<?php $this->options->themeUrl('/js/typecho.css');?>"></script>
