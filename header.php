8d9c6f6952f29132cccbb623ffa5731faa88bf55
安和 <42889600+AHCorn@users.noreply.github.com>
2020-03-31 16:58:13 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<div class="typecho-head-nav clearfix" role="navigation">
+    <nav id="typecho-nav-list">
+    <ul class="root">
+	<li class="parent"><a href="<?php $this->options->siteUrl(); ?>">概要</a></li>
+	</ul>
+<ul class="root">
+	<li class="parent"><a href="/p">文章</a></li>
+</ul>
+<?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+<?php while($pages->next()): ?>
+<ul class="root">
+<li class="parent"><a href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></li>
+</ul>
+<?php endwhile; ?>
